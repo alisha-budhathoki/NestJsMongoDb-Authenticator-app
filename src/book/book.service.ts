07fe46631a7c4a6d99a9ed07b2d06c044854839c
Alisha <alisha@Alishas-MacBook-Pro.local>
2023-12-16 21:53:25 +0545

    Add validation in create book and update book

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, Res } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
 import { Book } from 'src/book/schemas/book.schema';
@@ -36,7 +36,13 @@ export class BookService {
         return newBook;
     }
 
+
     async findById(id: string): Promise<Book> {
+        const isValidId = mongoose.isValidObjectId(id);
+        if (!isValidId) {
+            throw new BadRequestException('Please enter correct id.')
+        }
+
         const book = await this.bookModel.findById(id);
 
         if (!book) {
@@ -45,12 +51,14 @@ export class BookService {
         return book;
     }
 
+
     async updateById(id: string, book: Book): Promise<Book> {
         return await this.bookModel.findByIdAndUpdate(id, book, {
             new: true,
             runValidators: true,
         });
     }
+
 
     async deleteById(id: string): Promise<Book> {
         return await this.bookModel.findByIdAndDelete(id);
